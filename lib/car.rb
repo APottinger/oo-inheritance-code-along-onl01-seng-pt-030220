5b9f8c87dfde42c804f8e3844551b78181737ff8
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-04-10 23:07:51 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -3,6 +3,10 @@ require_relative "./vehicle.rb"
 class Car < Vehicle
  
  def go 
+<<<<<<< HEAD
    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+=======
+   "VR"
+>>>>>>> f4955b248fa3a045d794bf2897cdf85d602b0fad
  end 
 end
